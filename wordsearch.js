c559f7a7f57b7394e56b2fce21a608f3aea910fa
Nicole Law <nicole.law12@gmail.com>
2023-03-24 14:46:34 -0400

    pair programming word search, completed vertical and horizontal backwards

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,55 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Partners: Anastasia Zaika & Nicole Law
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if ((letters.length === 0) || (!word)) {
+    return false;
+  }
+
+  //finding horizontal word
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  // console.log("horizontalJoin",horizontalJoin);
+  for (l of horizontalJoin) {
+    //console.log("l", l);
+    if (l.includes(word)) return true;
+  }
+
+  //finding horizontal word backwards
+  const horizontalReverse = letters.map((ls) => ls.reverse().join(""));
+  for (l of horizontalReverse) {
+    //console.log("l", l);
+    if (l.includes(word)) return true;
+  }
+
+  // for (let i = 0; i < letters.length; i++) {
+  //   if (letters[i].reverse().join("").includes(word)) {
+  //     return true;
+  //   }
+  // }
+
+  //creating vertical array
+  const verticalArr = [];
+  for (let i = 0; i <= letters[0].length; i++) {
+    verticalArr.push([]);
+    //console.log("verticalArr", verticalArr);
+    for (let j = 0; j < letters.length; j++) {
+      verticalArr[i].push(letters[j][i]);
+    }
+  }
+
+  //finding vertical word
+  const verticalJoin = verticalArr.map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  //finding vertical word backwards
+  for (let i = 0; i < verticalArr.length; i++) {
+    if (verticalArr[i].reverse().join("").includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
